d9cd032fd4da6e0ff95868672a5d6bd0a1fac305
rizsi <andras.schmidt.hu@gmail.com>
2019-10-07 22:42:14 +0200

    SMS forwarder

---
+++ SMSForwarder/app/src/main/java/com/rizsi/smsforwarder/MainActivity.java
@@ -0,0 +1,177 @@
+package com.rizsi.smsforwarder;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.provider.Telephony;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String keySourceToForward="source.txt";
+    public static final String keyTargetPhone="target.txt";
+    public static final String keyShowAll="showall.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CheckBox checkBoxShowAllSMSNumber=(CheckBox)findViewById(R.id.checkBoxShowAllSMSNumber);
+        boolean savedValue="true".equals(loadKey(this, keyShowAll));
+        checkBoxShowAllSMSNumber.setChecked(savedValue);
+        checkBoxShowAllSMSNumber.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                saveKey(MainActivity.this, keyShowAll, ""+isChecked);
+            }
+        });
+        EditText sourceToForward = (EditText)findViewById(R.id.sourceToForward);
+        sourceToForward.setText(loadKey(this, keySourceToForward));
+        sourceToForward.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                saveKey(MainActivity.this, keySourceToForward, s.toString());
+//                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+        EditText targetPhone = (EditText)findViewById(R.id.targetPhone);
+        targetPhone.setText(loadKey(this, keyTargetPhone));
+        targetPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                saveKey(MainActivity.this, keyTargetPhone, s.toString());
+//                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void onRequirePermissions(View v) {
+//        Toast.makeText(this, "HELLO!", Toast.LENGTH_SHORT).show();
+        askPermission(Manifest.permission.RECEIVE_SMS);
+        askPermission(Manifest.permission.SEND_SMS);
+        askPermission(Manifest.permission.READ_PHONE_STATE);
+        Toast.makeText( this, "All permission checked!", Toast.LENGTH_SHORT).show();
+    }
+    private void askPermission(String perm)
+    {
+        if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+            Log.w("", "No permission for: "+perm);
+            ActivityCompat.requestPermissions(this, new String[]{perm}, 0);
+        }
+    }
+    public void onSendSMS(View v) {
+        sendSms(this, "Test message!");
+    }
+
+    public static void sendSms(Context c, String message)
+    {
+        String target=loadKey(c, keyTargetPhone);
+        if(target.length()>0) {
+            try {
+                SmsManager.getDefault().sendTextMessage(target, null, message, null, null);
+                Toast.makeText(c, "SMS forwarded to: " + target, Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Log.e("", "Error sending SMS", e);
+                Toast.makeText(c, "Cannot send SMS: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }else
+        {
+            Toast.makeText(c, "Cannot send SMS: target number is not set", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public static void smsReceived(Context c,String sender, String message) {
+        String sourceToForward=loadKey(c, keySourceToForward);
+        if("true".equals(loadKey(c, keyShowAll)))
+        {
+            Toast.makeText(c, "SMS from: "+sender+"\n"+"SMSFilter: "+sourceToForward, Toast.LENGTH_SHORT).show();
+        }
+        if(sourceToForward.length()>0)
+        {
+            if(sender.equals(sourceToForward))
+            {
+                sendSms(c, message);
+            }
+        }
+    }
+
+    public static void saveKey(Context c, String key, String value)
+    {
+        try {
+            Log.d("DATA",  "Save key: "+key+" "+value);
+            FileOutputStream fOut=c.openFileOutput(key, Context.MODE_PRIVATE);
+            try {
+                Writer wr=new OutputStreamWriter(fOut, "UTF-8");
+                wr.write(value);
+                wr.close();
+            }finally
+            {
+                fOut.close();
+            }
+        }catch(Exception e)
+        {
+            Log.e("DATA",  "Save key: "+key+" "+value, e);
+        }
+    }
+    public static String loadKey(Context c, String key)
+    {
+        try {
+            Log.d("DATA",  "Load key: "+key);
+            InputStream is=c.openFileInput(key);
+            try {
+                Reader r = new InputStreamReader(is, "UTF-8");
+                StringBuilder sb=new StringBuilder();
+                int ch;
+                while((ch=r.read())!=-1)
+                {
+                    sb.append((char)ch);
+                }
+                return sb.toString();
+            } finally {
+                is.close();
+            }
+        }catch(Exception e)
+        {
+            Log.e("DATA",  "Load key: "+key, e);
+        }
+        return "";
+    }
+}
---
+++ SMSForwarder/app/src/main/java/com/rizsi/smsforwarder/SmsReceiver.java
@@ -0,0 +1,42 @@
+package com.rizsi.smsforwarder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                // get sms objects
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus.length == 0) {
+                    return;
+                }
+                // large message might be broken into many
+                SmsMessage[] messages = new SmsMessage[pdus.length];
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    sb.append(messages[i].getMessageBody());
+                }
+                String sender = messages[0].getOriginatingAddress();
+                String message = sb.toString();
+//                Toast.makeText(context, sender, Toast.LENGTH_SHORT).show();
+                MainActivity.smsReceived(context, sender, message);
+                // prevent any other broadcast receivers from receiving broadcast
+                // abortBroadcast();
+//                SmsManager.getDefault().sendTextMessage("+36303786914",null,message,null,null);
+            }
+        }
+    }
+}
